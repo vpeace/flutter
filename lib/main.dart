cd510f91c4a48e258dfe6053a07a613982d2c77a
vpeace <amandeep.arora67@gmail.com>
2019-06-17 23:51:02 +0530

    APi Authorization added

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
+const _headerAuthorization = "Authorization";
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -16,11 +16,13 @@ class MyApp extends StatelessWidget {
           title: Text(title),
         ),
         body: Center(
-          child: FadeInImage.assetNetwork(
-            placeholder: 'assets/tenor.gif',
-            image :
-            'https://picsum.photos/250?image=8',
-//            errorWidget: new Icon(Icons.error),
+          child: FadeInImage(
+            placeholder: AssetImage('assets/tenor.gif'),
+            image : NetworkImage(
+                'http://via70.uplltd.com/CitizenConnectApp/api/department/downloadDepartmentFileForUser?departmentId=135',
+                headers: {_headerAuthorization: "Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiI3ODMyM2RmMy0zZjhhLTQxZmMtYWQ3Yi1jOWIxYzk5Njk2MzQiLCJzdWIiOiJjYy1qd3QiLCJ1c2VyaWQiOjIwNCwiaWF0IjoxNTQ4MDU3NjYxLCJleHAiOjE1NjUzMzc2NjF9.QEwZO8jokPMQGtYNTTi033W1rfp5sWaPEP7kWQbVBHeAZW9jiKlMUtsn33eOD4HGPJcGg5u0o_1-Ye6P4Jlz3Q"
+                }),
+// errorWidget: new Icon(Icons.error),
           ),
         ),
       ),
